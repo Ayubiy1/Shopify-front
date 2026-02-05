7a57683fa783e4b68350f61cc1902cd0abe5de62
Ayubiy1 <ayubiyurinboyev@email.com>
2026-02-05 12:44:23 +0500

    fixed: 27

--- src/context/appWrite.js
+++ src/context/appWrite.js
@@ -1,15 +1,15 @@
-import { Client, Account } from "appwrite";
-
-const client = new Client()
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("696e106c000b9f41a7af");
-
-export const account = new Account(client);
-
 // import { Client, Account } from "appwrite";
 
 // const client = new Client()
 //   .setEndpoint("https://cloud.appwrite.io/v1")
-//   .setProject("PROJECT_ID"); // Appwrite Project ID
+//   .setProject("696e106c000b9f41a7af");
 
 // export const account = new Account(client);
+
+// // import { Client, Account } from "appwrite";
+
+// // const client = new Client()
+// //   .setEndpoint("https://cloud.appwrite.io/v1")
+// //   .setProject("PROJECT_ID"); // Appwrite Project ID
+
+// // export const account = new Account(client);
