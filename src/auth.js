7438829f0c97c84526b75be795ce2f4f8f98d519
Ayubiy1 <ayubiyurinboyev@email.com>
2026-02-08 11:14:02 +0500

    fixed: 31

--- src/auth.js
+++ src/auth.js
@@ -5,8 +5,8 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const accessToken = localStorage.getItem("token");
-  if (accessToken) {
+  const token = localStorage.getItem("token");
+  if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
